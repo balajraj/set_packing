89a04f133466fef8503ea85f2e32795aba8aa5b9
balajraj <brajagop@gmail.com>
2018-07-15 15:27:39 +0200

    fixed errors in inputdriver

--- src/main/java/com/setpacking/InputDriver.java
+++ src/main/java/com/setpacking/InputDriver.java
@@ -5,40 +5,62 @@ public class InputDriver {
 
    public static void main(String [] args) {
        Scanner scan = new Scanner(System.in);
-       int rowSize = scan.nextInt();
-       int numRows = scan.nextInt();
-       String line = scan.nextLine();
+       int rowSize = 0;
+       int numRows = 0;
        Map<Integer,Stack<Group>> winGrpPref = new HashMap<>();
        Map<Integer,Stack<Group>> noGrpPref = new HashMap<>();
-       while( line != null ) {
+       int lineCnt=0;
+       while( scan.hasNextLine() ) {
+           String line = scan.nextLine();
+           //System.out.println(line);
            String [] groupInfo = line.split(" ");
-           List<Passenger> passengers = new ArrayList<Passenger>();
-           boolean winPerf =false;
-           for(int i=0; i < groupInfo.length; ++i) {
-               String elem = groupInfo[i];
-               if(elem.charAt(elem.length()-1) == 'W') {
-                   winPerf =true;
-                   Integer id = Integer.parseInt(elem.substring(0,elem.length()-2));
-                   Passenger p = new Passenger(winPref,id);
-                   passengers.add(p);
-               }
-               Integer id = Integer.parseInt(elem);
-               Passenger p = new Passenger(false,id);
-               passengers.add(p);
-           }
-           Group grp = new Group(passengers);
-           if (winPref) {
-               addElementsToMap(winGrpPref,grp) ;
+           if (lineCnt == 0 ) {
+               rowSize = Integer.parseInt(groupInfo[0]);
+               numRows = Integer.parseInt(groupInfo[1]);
+               lineCnt++;
            }
            else {
-               addElementsToMap(noGrpPref,grp);
+               List<Passenger> passengers = new ArrayList<Passenger>();
+               boolean windowPerf =false;
+               for(int i=0; i < groupInfo.length; ++i) {
+                   String elem = groupInfo[i];
+                   //System.out.println(elem+ " " + elem.length());
+                   if(elem.charAt(elem.length()-1) == 'W') {
+                       windowPerf =true;
+                       int len = elem.length()-1;
+                       //System.out.println("inside w "+len);
+                       String digit ="";
+                       if(len == 0 ) {
+                           digit = ""+elem.charAt(0);
+                       }
+                       else {
+                           digit = elem.substring(0,len);
+                       }
+                       //System.out.println(digit);
+                       Integer id = Integer.parseInt(digit);
+                       Passenger p = new Passenger(windowPerf,id);
+                       passengers.add(p);
+                   }
+                   else {
+                       Integer id = Integer.parseInt(elem);
+                       Passenger p = new Passenger(false,id);
+                       passengers.add(p);
+                   }
+               }
+               Group grp = new Group(passengers);
+               if (windowPerf) {
+                   addElementsToMap(winGrpPref,grp) ;
+               }
+               else {
+                   addElementsToMap(noGrpPref,grp);
+               }
            }
-           line = scan.nextLine();
        }
        SetPackingAlgo setpack = new SetPackingAlgo(rowSize,numRows,rowSize);
        double satisfaction = setpack.getAllocationSatisfaction(winGrpPref,noGrpPref);
        setpack.printAssignment();
        System.out.println((int)satisfaction * 100+"%");
+
 
    }
 
